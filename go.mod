30ac41c8a8defe65990c2e64d08623cc3f2db5b4
Reyco Seguma <segumareyco@gmail.com>
2025-08-18 02:09:16 +0800

    Replaced goose migration with manual gorm

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.3
 
 require (
 	github.com/go-chi/chi/v5 v5.2.2
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
 	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
@@ -18,7 +19,6 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496 // indirect
-	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
--- internal/database/migration/004_create_apis_table.go
+++ internal/database/migration/004_create_apis_table.go
@@ -1,7 +1,14 @@
--- +goose Up
+package migration
 
+import (
+	"log"
+
+	"gorm.io/gorm"
+)
+
+func CreateAPITable(db *gorm.DB) {
+	sql := `
 -- CREATE TABLE
--- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS apis (
     api_id              SERIAL PRIMARY KEY,
     api_uuid            UUID NOT NULL UNIQUE,
@@ -17,43 +24,23 @@ CREATE TABLE IF NOT EXISTS apis (
     created_at          TIMESTAMPTZ DEFAULT now(),
     updated_at          TIMESTAMPTZ DEFAULT now()
 );
--- +goose StatementEnd
 
 -- ADD CONSTRAINTS
--- +goose StatementBegin
 ALTER TABLE apis
     ADD CONSTRAINT fk_apis_service_id FOREIGN KEY (service_id) REFERENCES services(service_id) ON DELETE CASCADE;
 ALTER TABLE apis
     ADD CONSTRAINT fk_apis_auth_container_id FOREIGN KEY (auth_container_id) REFERENCES auth_containers(auth_container_id) ON DELETE CASCADE;
--- +goose StatementEnd
 
 -- ADD INDEXES
--- +goose StatementBegin
 CREATE INDEX idx_apis_api_name ON apis (api_name);
 CREATE INDEX idx_apis_api_type ON apis (api_type);
 CREATE INDEX idx_apis_identifier ON apis (identifier);
 CREATE INDEX idx_apis_service_id ON apis (service_id);
 CREATE INDEX idx_apis_auth_container_id ON apis (auth_container_id);
--- +goose StatementEnd
+`
+	if err := db.Exec(sql).Error; err != nil {
+		log.Fatalf("❌ Failed to run migration 004_create_apis_table: %v", err)
+	}
 
--- +goose Down
-
--- DROP INDEXES
--- +goose StatementBegin
-DROP INDEX IF EXISTS idx_apis_api_name;
-DROP INDEX IF EXISTS idx_apis_api_type;
-DROP INDEX IF EXISTS idx_apis_identifier;
-DROP INDEX IF EXISTS idx_apis_service_id;
-DROP INDEX IF EXISTS idx_apis_auth_container_id;
--- +goose StatementEnd
-
--- DROP CONSTRAINTS
--- +goose StatementBegin
-ALTER TABLE apis DROP CONSTRAINT IF EXISTS fk_apis_service_id;
-ALTER TABLE apis DROP CONSTRAINT IF EXISTS fk_apis_auth_container_id;
--- +goose StatementEnd
-
--- DROP TABLE
--- +goose StatementBegin
-DROP TABLE IF EXISTS apis;
--- +goose StatementEnd
+	log.Println("✅ Migration 004_create_apis_table executed")
+}
