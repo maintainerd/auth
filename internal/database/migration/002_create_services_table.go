cd19445eade672eb88f9564fe258503d45b44222
Reyco Seguma <segumareyco@gmail.com>
2025-09-04 15:11:09 +0800

    Services implementation initial

--- internal/database/migration/002_create_services_table.go
+++ internal/database/migration/002_create_services_table.go
@@ -12,12 +12,11 @@ func CreateServiceTable(db *gorm.DB) {
 CREATE TABLE IF NOT EXISTS services (
     service_id      SERIAL PRIMARY KEY,
     service_uuid    UUID NOT NULL UNIQUE,
-    service_name    VARCHAR(100) NOT NULL,
+    name    				VARCHAR(100) NOT NULL,
     display_name    TEXT NOT NULL,
     description     TEXT NOT NULL,
     service_type    TEXT NOT NULL,
     version         VARCHAR(20) NOT NULL,
-    config          JSONB,
     is_active       BOOLEAN DEFAULT FALSE,
     is_default      BOOLEAN DEFAULT FALSE,
 		is_public				BOOLEAN DEFAULT FALSE,
@@ -27,7 +26,7 @@ CREATE TABLE IF NOT EXISTS services (
 
 -- ADD INDEXES
 CREATE INDEX IF NOT EXISTS idx_services_service_uuid ON services (service_uuid);
-CREATE INDEX IF NOT EXISTS idx_services_service_name ON services (service_name);
+CREATE INDEX IF NOT EXISTS idx_services_name ON services (name);
 CREATE INDEX IF NOT EXISTS idx_services_display_name ON services (display_name);
 CREATE INDEX IF NOT EXISTS idx_services_service_type ON services (service_type);
 CREATE INDEX IF NOT EXISTS idx_services_is_active ON services (is_active);
