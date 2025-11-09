02b69965c318c7a374ffd1eb8976d646d46092a1
Reyco Seguma <segumareyco@gmail.com>
2025-11-09 11:21:17 +0800

    Added fullname to users table and dtos for request

--- internal/dto/user_dto.go
+++ internal/dto/user_dto.go
@@ -13,6 +13,7 @@ import (
 type UserResponseDto struct {
 	UserUUID           uuid.UUID                  `json:"user_uuid"`
 	Username           string                     `json:"username"`
+	Fullname           string                     `json:"fullname"`
 	Email              string                     `json:"email"`
 	Phone              string                     `json:"phone"`
 	IsEmailVerified    bool                       `json:"is_email_verified"`
@@ -40,6 +41,7 @@ type UserIdentityResponseDto struct {
 // User input structures
 type UserCreateRequestDto struct {
 	Username   string  `json:"username"`
+	Fullname   string  `json:"fullname"`
 	Email      *string `json:"email,omitempty"`
 	Phone      *string `json:"phone,omitempty"`
 	Password   string  `json:"password"`
@@ -49,6 +51,7 @@ type UserCreateRequestDto struct {
 func (dto UserCreateRequestDto) Validate() error {
 	return validation.ValidateStruct(&dto,
 		validation.Field(&dto.Username, validation.Required, validation.Length(3, 50)),
+		validation.Field(&dto.Fullname, validation.Required, validation.Length(1, 255)),
 		validation.Field(&dto.Email, validation.When(dto.Email != nil, is.Email)),
 		validation.Field(&dto.Phone, validation.When(dto.Phone != nil, validation.Length(10, 20))),
 		validation.Field(&dto.Password, validation.Required, validation.Length(8, 100)),
@@ -58,6 +61,7 @@ func (dto UserCreateRequestDto) Validate() error {
 
 type UserUpdateRequestDto struct {
 	Username string  `json:"username"`
+	Fullname string  `json:"fullname"`
 	Email    *string `json:"email,omitempty"`
 	Phone    *string `json:"phone,omitempty"`
 }
@@ -65,6 +69,7 @@ type UserUpdateRequestDto struct {
 func (dto UserUpdateRequestDto) Validate() error {
 	return validation.ValidateStruct(&dto,
 		validation.Field(&dto.Username, validation.Required, validation.Length(3, 50)),
+		validation.Field(&dto.Fullname, validation.Required, validation.Length(1, 255)),
 		validation.Field(&dto.Email, validation.When(dto.Email != nil, is.Email)),
 		validation.Field(&dto.Phone, validation.When(dto.Phone != nil, validation.Length(10, 20))),
 	)
