77fcd3adb3baae3a8f94cae82c45f7b4b3329125
Reyco Seguma <segumareyco@gmail.com>
2026-01-01 08:45:00 +0800

    Added api tenant constraint

--- internal/database/migration/007_create_apis_table.go
+++ internal/database/migration/007_create_apis_table.go
@@ -12,12 +12,13 @@ func CreateAPITable(db *gorm.DB) {
 CREATE TABLE IF NOT EXISTS apis (
     api_id					SERIAL PRIMARY KEY,
     api_uuid				UUID NOT NULL UNIQUE,
+    tenant_id       INTEGER NOT NULL,
+    service_id			INTEGER NOT NULL,
     name            VARCHAR(100) NOT NULL,
     display_name		TEXT NOT NULL,
 		description			TEXT NOT NULL,
     api_type				TEXT NOT NULL,
     identifier			TEXT NOT NULL,
-		service_id			INTEGER NOT NULL,
     status					TEXT DEFAULT 'inactive' CHECK (status IN ('active', 'inactive')),
     is_default			BOOLEAN DEFAULT FALSE,
     is_system				BOOLEAN DEFAULT FALSE,
@@ -35,15 +36,24 @@ BEGIN
             ADD CONSTRAINT fk_apis_service_id FOREIGN KEY (service_id)
             REFERENCES services(service_id) ON DELETE CASCADE;
     END IF;
+
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_constraint WHERE conname = 'fk_apis_tenant_id'
+    ) THEN
+        ALTER TABLE apis
+            ADD CONSTRAINT fk_apis_tenant_id FOREIGN KEY (tenant_id)
+            REFERENCES tenants(tenant_id) ON DELETE CASCADE;
+    END IF;
 END$$;
 
 -- ADD INDEXES
-CREATE INDEX IF NOT EXISTS idx_apis_uuid ON apis (api_uuid);
+CREATE INDEX IF NOT EXISTS idx_apis_api_uuid ON apis (api_uuid);
 CREATE INDEX IF NOT EXISTS idx_apis_name ON apis (name);
 CREATE INDEX IF NOT EXISTS idx_apis_display_name ON apis (display_name);
 CREATE INDEX IF NOT EXISTS idx_apis_api_type ON apis (api_type);
 CREATE INDEX IF NOT EXISTS idx_apis_identifier ON apis (identifier);
 CREATE INDEX IF NOT EXISTS idx_apis_service_id ON apis (service_id);
+CREATE INDEX IF NOT EXISTS idx_apis_tenant_id ON apis (tenant_id);
 CREATE INDEX IF NOT EXISTS idx_apis_status ON apis (status);
 CREATE INDEX IF NOT EXISTS idx_apis_is_default ON apis (is_default);
 CREATE INDEX IF NOT EXISTS idx_apis_is_system ON apis (is_system);
