693bc37a3409df8e37a2968cefdd20db45defc27
Reyco Seguma <segumareyco@gmail.com>
2026-01-02 11:04:06 +0800

    Added signup flow tenant constraint

--- internal/database/migration/027_create_signup_flows_table.go
+++ internal/database/migration/027_create_signup_flows_table.go
@@ -11,7 +11,8 @@ func CreateSignupFlowTable(db *gorm.DB) {
 -- CREATE TABLE
 CREATE TABLE IF NOT EXISTS signup_flows (
     signup_flow_id   	SERIAL PRIMARY KEY,
-    signup_flow_uuid		UUID NOT NULL UNIQUE,
+    signup_flow_uuid  UUID NOT NULL UNIQUE,
+    tenant_id					BIGINT NOT NULL,
     name							VARCHAR(100) NOT NULL,
     description				TEXT NOT NULL,
 		identifier				VARCHAR(255) NOT NULL UNIQUE,
@@ -26,6 +27,14 @@ CREATE TABLE IF NOT EXISTS signup_flows (
 DO $$
 BEGIN
     IF NOT EXISTS (
+        SELECT 1 FROM pg_constraint WHERE conname = 'fk_signup_flows_tenant_id'
+    ) THEN
+        ALTER TABLE signup_flows
+            ADD CONSTRAINT fk_signup_flows_tenant_id FOREIGN KEY (tenant_id)
+            REFERENCES tenants(tenant_id) ON DELETE CASCADE;
+    END IF;
+
+    IF NOT EXISTS (
         SELECT 1 FROM pg_constraint WHERE conname = 'fk_signup_flows_auth_client_id'
     ) THEN
         ALTER TABLE signup_flows
@@ -36,6 +45,7 @@ END$$;
 
 -- ADD INDEXES
 CREATE INDEX IF NOT EXISTS idx_signup_flow_uuid ON signup_flows (signup_flow_uuid);
+CREATE INDEX IF NOT EXISTS idx_signup_flow_tenant_id ON signup_flows (tenant_id);
 CREATE INDEX IF NOT EXISTS idx_signup_flow_name ON signup_flows (name);
 CREATE INDEX IF NOT EXISTS idx_signup_flow_identifier ON signup_flows (identifier);
 CREATE INDEX IF NOT EXISTS idx_signup_flow_status ON signup_flows (status);
